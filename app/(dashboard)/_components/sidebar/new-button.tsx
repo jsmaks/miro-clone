bb1c5632100afa8d98f58371192650311f297eb9
jsmaks <jsmaks26@gmail.com>
2024-06-26 16:43:15 +0300

    f

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -7,13 +7,16 @@ import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
 const NewButton = () => {
   return (
     <Dialog>
-          <DialogTrigger asChild>
-              <div className='aspect-square'>
-                  <button className='bg-white/25 h-full w-full'>
-                      <Plus className='text-white' />
-                  </button>
-              </div>
+      <DialogTrigger asChild>
+        <div className="aspect-square">
+          <button className="flex h-full w-full items-center justify-center rounded-md bg-white/25 opacity-60 transition hover:opacity-100">
+            <Plus className="text-white" />
+          </button>
+        </div>
       </DialogTrigger>
+      <DialogContent>
+        <CreateOrganization />
+      </DialogContent>
     </Dialog>
   );
 };
